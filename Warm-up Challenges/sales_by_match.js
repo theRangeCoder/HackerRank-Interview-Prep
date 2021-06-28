88963d8ae1471fa454eb9c69f30430b079a569f8
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-28 23:22:23 +0200

    Added the solution file for the first problem

---
+++ Warm-up Challenges/sales_by_match.js
@@ -0,0 +1,2 @@
+/* The problem statement for this challenge can be viewed at: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup */
+
