c1f9a35da6cfc64b6b76d66ac8eb1c780d148bae
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-28 23:30:26 +0200

    Added the template of the function to be written

--- Warm-up Challenges/sales_by_match.js
+++ Warm-up Challenges/sales_by_match.js
@@ -1,2 +1,15 @@
 /* The problem statement for this challenge can be viewed at: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup */
 
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    // Write your code here
+
+}
