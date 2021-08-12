f416703a28d724b1cc3999563953b358f76f221b
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-08-12 22:56:56 +0200

    Repeated strings: boilerplate

---
+++ Warm-up Challenges/repeated_string.js
@@ -0,0 +1,60 @@
+/* The problem statement for this challenge can be viewed at: 
+https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup */
+
+// The boilerplate (lines 6-30 and 54-68) has been taken from the above source as well
+
+/*
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+*/
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+function repeatedString(s, n) {
+    // Write your code here
+
+}
+
+/*
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = repeatedString(s, n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+*/
